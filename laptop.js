fc240da727be0154f0dd7d4a69b73a6c05691e79
Ashvika123 <120046465+Ashvika123@users.noreply.github.com>
2023-11-28 18:08:45 +0000

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,34 @@
+img = "";
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center
+}
+function preload() {
+img = loadImage('laptop.jpg');
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Laptop", 45, 75 );
+    noFill();
+    stroke("#FF0000")
+    rect(45, 55 , 450, 365);
+
+    
+    
+}
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
